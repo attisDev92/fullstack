a2390104f18a2db6fc3457b47f6b43b6420535b5
attisDev92 <attis.alejandro@gmail.com>
2023-10-14 17:52:48 -0500

    part1 exercises 1.9

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -5,17 +5,37 @@ const root = createRoot(document.getElementById('container'));
 
 const Button = ({ nameBtn, handlerClick}) => <button onClick={handlerClick}>{nameBtn}</button>;
 
-const Statistics = ({ name, number }) => <p>{name} {number}</p>
+const Display = ({ name, number }) => <p>{name} {number}</p>
+
+const Statiscs = ({ good, neutral, bad }) => {
+  
+  let total = good + neutral + bad;
+  let promedio = total === 0 ? 0 : (good - bad) / total;
+  let positiveValue = total === 0 ? '0%' : `${(good * 100) / total}%`;
+
+  if (total === 0) {
+    return <div>No feedback given</div>
+  };
+
+  return (
+    <div>
+      <h2>Statiscs</h2>
+      <Display name='good' number={good} />
+      <Display name='neutral' number={neutral} />
+      <Display name='bad' number={bad} />
+      <Display name='Total' number={total}/>
+      <Display name='Averege' number={promedio}/>
+      <Display name='Positive' number={positiveValue}/>
+    </div>
+  )
+
+}
 
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-
-  let total = good + neutral + bad;
-  let promedio = total === 0 ? 0 : (good - bad) / total;
-  let positiveValue = total === 0 ? '0%' : `${(good * 100) / total}%`;
 
   const handlerClickGood = () => {
     setGood(good + 1);
@@ -38,13 +58,7 @@ const App = () => {
         <Button nameBtn='bad' handlerClick={handlerClickBad} />
       </div>
       <div>
-        <h2>Statiscs</h2>
-        <Statistics name='good' number={good} />
-        <Statistics name='neutral' number={neutral} />
-        <Statistics name='bad' number={bad} />
-        <Statistics name='Total' number={total}/>
-        <Statistics name='Averege' number={promedio}/>
-        <Statistics name='Positive' number={positiveValue}/>
+        <Statiscs good={good} neutral={neutral} bad={bad} />
       </div>
     </div>
   )
