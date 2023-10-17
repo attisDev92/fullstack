a3ddf7fef14d3f63a8b5e17eb78e07af745e9048
attisDev92 <attis.alejandro@gmail.com>
2023-10-16 23:41:45 -0500

    part2 exercises 2.4 y 2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,4 +1,4 @@
-
+import Courses from './Courses';
 
 const Header = ({ name }) => <h1>{name}</h1>;
 
@@ -32,18 +32,26 @@ const Content = ({ parts }) => {
 	)
 }
 
-const Course = ({ name, parts }) => {
+const Course = ({ courses }) => {
 	return (
 		<div>
-			<Header name={name}/>
-			<Content parts={parts}/>
-			<Total parts={parts} />
-		</div>
+			{
+				courses.map( course => {
+					return (
+						<div key={course.id}>
+							<Header name={course.name}/>
+							<Content parts={course.parts}/>
+							<Total parts={course.parts} />
+						</div>
+					)
+				})
+			}
+		</div>	
 	);
 };
 
-const App = ({ course }) => {
-	return <Course name={course.name} parts={course.parts} />;
+const App = () => {
+	return <Course courses={Courses} />;
 }
 
 export default App;
--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -2,27 +2,7 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-const course = {
-	id: 1,
-	name: 'Half Stack application development',
-	parts: [
-		{
-			name: 'Fundamentals of React',
-			exercises: 10,
-			id: 1,
-		},
-		{
-			name: 'Using props to pass data',
-			exercises: 7,
-			id: 2,
-		},
-		{
-			name: 'State of a component',
-			exercises: 14,
-			id: 3,
-		},
-	],
-}
+
 
 const root = createRoot(document.getElementById('container'));
-root.render( <App course={course}/>);
+root.render( <App />);
