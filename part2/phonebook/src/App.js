935a2d4c41cd68e10f617e9024e545adc7a847fc
attisDev92 <attis.alejandro@gmail.com>
2023-10-17 20:54:19 -0500

    part2 exercises 2.7

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,63 @@
+import {useState} from 'react';
+import Persons from './components/Persons';
+
+const Input = ({ type, onChange }) => <input type={type} onChange={onChange} />;
+
+const Button = ({ nameBtn, type }) => <button type={type}>{nameBtn}</button>;
+
+const Person = ({ person }) => {
+    return (
+       <li>
+            <p>{person.name}</p>
+       </li> 
+    )
+};
+
+const App = () => {
+
+    const [persons, setPersons] = useState(Persons);
+    const [newName, setNewName] = useState('');
+
+    const handlerOnChange = (event) => {
+        setNewName(event.target.value);
+    };
+
+    const handlerOnSubmit = (event) => {
+        event.preventDefault();
+        
+        const objectName = {
+            name: newName
+        };
+
+        const newPersons = [
+            ...persons,
+            objectName
+        ];
+
+        setPersons(newPersons);
+        setNewName('');
+    }
+
+    return (
+        <div>
+            <h1> PHONEBOOK </h1>
+            <div>
+                <form onSubmit={handlerOnSubmit}>
+                    <label>name: </label>
+                    <Input type='text' onChange={handlerOnChange}/>
+                    <Button nameBtn="add" type="onSubmit"/>
+                </form>                
+            </div>
+            <h2> Numbers </h2>
+            <ul>
+                {persons.map(
+                    person => <Person key={person.name} person={person}/>)
+                }
+            </ul>
+            
+        </div>
+    )
+
+}
+
+export default App;
---
+++ part2/phonebook/src/components/Persons.js
@@ -0,0 +1,6 @@
+const Persons = [
+    {name: "Arto Hellos"},
+    {name: "Attis Burnham"}
+];
+
+export default Persons;
