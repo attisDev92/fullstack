f08bf1ff6f650d90cf5b75dfffe720944400afef
attisDev92 <attis.alejandro@gmail.com>
2023-10-26 20:03:42 -0500

    part3 exercises 3.9

--- part3/backend phonebook/index.js
+++ part3/backend phonebook/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const PORT =  3001;
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan(':method :url :status :response-time ms'));
+app.use(cors());
 
 let persons = [
     {
