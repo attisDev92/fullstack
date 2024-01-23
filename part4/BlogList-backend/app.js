31359847049673322b3d0146c1a1f5a0005ce2b6
attisDev92 <attis.alejandro@gmail.com>
2024-01-23 13:02:39 -0500

    part 4, exerc 4.2

--- part4/BlogList-backend/app.js
+++ part4/BlogList-backend/app.js
@@ -22,8 +22,12 @@ app.use(morgan(':method :url :status :response-time ms'));
 app.use(cors());
 app.use(express.static('build'));
 app.use(express.json());
+app.use(middleware.requestLogger);
 
 app.use('/api/blogs', blogsRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;
 
