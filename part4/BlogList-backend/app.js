f1f1a1e3d906dfffb77c12e2f642b79eab21dfcc
attisDev92 <attis.alejandro@gmail.com>
2024-01-23 12:10:38 -0500

    part 4, exerc 4.2.2

---
+++ part4/BlogList-backend/app.js
@@ -0,0 +1,29 @@
+const config = require('./utils/config');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const blogsRouter = require('./controllers/blogs');
+const middleware = require('./utils/middleware');
+const logger = require('./utils/logger');
+const mongoose = require('mongoose');
+const morgan = require('morgan')
+
+logger.info('connecting to ', config.MONGOBD);
+
+mongoose.connect(config.MONGOBD)
+    .then(() => {
+        logger.info('connected to MonfoDB')
+    })
+    .catch(error => {
+        logger.catch('error connecting database ', error.message)
+    });
+
+app.use(morgan(':method :url :status :response-time ms'));
+app.use(cors());
+app.use(express.static('build'));
+app.use(express.json());
+
+app.use('/api/blogs', blogsRouter);
+
+module.exports = app;
+
--- part4/BlogList-backend/index.js
+++ part4/BlogList-backend/index.js
@@ -1,36 +1,10 @@
-const logger = require('./utils/logger');
+const app = require('./app');
+const http = require('http');
 const config = require('./utils/config');
-const morgan = require('morgan')
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const Blog = require('./models/blog');
-const mongoose = require('mongoose');
+const logger = require('./utils/logger');
 
-mongoose.connect(config.MONGOBD);
+const server = http.createServer(app);
 
-app.use(morgan(':method :url :status :response-time ms'));
-app.use(cors());
-app.use(express.json());
-
-app.get('/api/blogs', (req, res) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      res.json(blogs)
-    })
-});
-
-app.post('/api/blogs', (req, res) => {
-  const blog = new Blog(req.body)
-
-  blog
-    .save()
-    .then(result => {
-      res.status(201).json(result)
-    })
-});
-
-app.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`);
+server.listen(config.PORT, () => {
+  logger.info(`server running on port ${config.PORT}`);
 });
